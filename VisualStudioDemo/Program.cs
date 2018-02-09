2067cb4ae361a2635183226b99942ce1f895a698
sudokarst <sudokarst@gmail.com>
2018-02-09 09:06:25 -0500

    demo add message

--- VisualStudioDemo/Program.cs
+++ VisualStudioDemo/Program.cs
@@ -6,7 +6,8 @@ namespace VisualStudioDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Visual Studio Demo!");
+            Console.ReadLine();
         }
     }
 }
